f1d52f3cd1912e108b7c8289f72461b0f492bcf1
Mohamed Shabeer K P <mohammedshabeerkp@gmail.com>
2023-01-10 03:03:41 +0700

    minor changes

--- Navigus_Assignment_2/wsgi.py
+++ Navigus_Assignment_2/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Navigus_Assignment_2.settings')
 
 application = get_wsgi_application()
